fca1b7999b0321d76eb8e676805de096a95d0a8a
Ayeon <dldkekdusz@naver.com>
2025-08-26 12:13:06 +0900

    검색 api > page optional로 받도록 수정

--- SeachBook/SeachBook/Service/API/ITBookAPI/ITBookAPI.swift
+++ SeachBook/SeachBook/Service/API/ITBookAPI/ITBookAPI.swift
@@ -9,20 +9,24 @@ import Foundation
 import Combine
 
 protocol ITBookAPIProtocol {
-    func searchBooks(text: String, page: Int) -> AnyPublisher<ITBookSearchResponseModel, APIError>
+    func searchBooks(text: String, page: Int?) -> AnyPublisher<ITBookSearchResponseModel, APIError>
     func getBookDetail(isbn13: String) -> AnyPublisher<ITBookDetailResponseModel, APIError>
 }
 
 struct ITBookAPI: ITBookAPIProtocol {
     enum EndPoint {
-        case searchBooks(text: String, page: Int)
+        case searchBooks(text: String, page: Int?)
         case bookDetail(isbn13: String)
 
         var url: String {
             let url = "https://api.itbook.store/1.0"
             switch self {
             case let .searchBooks(keyword, page):
-                return url + "/search/\(keyword)/\(page)"
+                if let page {
+                    return url + "/search/\(keyword)/\(page)"
+                } else {
+                    return url + "/search/\(keyword)"
+                }
 
             case let .bookDetail(isbn13):
                 return url + "/books/\(isbn13)"
@@ -32,7 +36,7 @@ struct ITBookAPI: ITBookAPIProtocol {
 }
 
 extension ITBookAPI {
-    func searchBooks(text: String, page: Int) -> AnyPublisher<ITBookSearchResponseModel, APIError> {
+    func searchBooks(text: String, page: Int?) -> AnyPublisher<ITBookSearchResponseModel, APIError> {
         let url = EndPoint.searchBooks(text: text, page: page).url
         return HttpAPIManager.callRequest(api: url, method: .get, responseType: ITBookSearchResponseModel.self)
     }
